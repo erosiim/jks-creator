2e985a86ecaa5d30f133776bd0dc2e09583d78e8
Alonso Escamilla <exkapp@gmail.com>
2023-10-11 19:22:19 -0600

    Actualiza Logger mensajes de error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,17 @@
 import utils.JksCreator;
 import utils.Texto;
 
-import java.awt.*;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+
+    private final static Logger LOGGER = Logger.getLogger("Main");
     public static void main(String[] args) throws GeneralSecurityException, IOException {
-        //JksCreator.generateJKS("src/main/resources/medium.crt"); //Recibe la ruta de donde se ubique el certificado a cargar
-        menu();
+       menu();
     }
 
     static void menu() throws GeneralSecurityException, IOException {
@@ -20,14 +22,14 @@ public class Main {
         try {
             System.out.println(Texto.INDICACION_NUMERO_CERTIFICADOS.getMensaje());
             numeroCertificados = sc.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println(Texto.INDICACION_SOLO_NUMEROS);
-        } finally {
             for (int i = 0; i < numeroCertificados; i++) {
                 System.out.println(Texto.INDICACION_RUTA_CERTIFICADOS.getMensaje() + (i + 1) + ": ");
                 certificadosMap.put(Texto.PREFIJO_CERTIFICADOS.getMensaje() + (i + 1), sc.next());
             }
             JksCreator.generateJKS(certificadosMap);
+        } catch (InputMismatchException e) {
+            LOGGER.log(Level.SEVERE, Texto.INDICACION_SOLO_NUMEROS.getMensaje());
+            LOGGER.log(Level.SEVERE, Texto.JKS_ERROR.getMensaje());
         }
     }
 }
